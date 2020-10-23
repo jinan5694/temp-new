2908de25a17d58f9397fdf3d596bfab97efea8b1
jinan <jinan5694@163.com>
2020-10-23 11:45:26 +0800

    ok

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,12 +3,20 @@ module.exports = {
   env: {
     node: true
   },
-  extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
+  extends: ['plugin:vue/essential', 'eslint:recommended', '@vue/prettier'],
   parserOptions: {
-    parser: "babel-eslint"
+    parser: 'babel-eslint'
   },
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
+    'prettier/prettier': [
+      'error',
+      {
+        semi: false, // 不已分号结尾
+        singleQuote: true // js中使用单引号
+      }
+    ],
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'vue/require-default-prop': 'off'
   }
-};
+}
